4afbf1729546666b376b432c8e2d67c8c53e2541
Kunal Rajput <92367095+kunalrajput7@users.noreply.github.com>
2024-09-04 16:25:48 -0500

    secrets.dart

--- lib/secrets.dart
+++ lib/secrets.dart
@@ -1,3 +1,3 @@
 // this is my secret api key generated from the OpenAI account.
 
-const openAIAPIKey = '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';
+const openAIAPIKey = 'enter your openai api key';
